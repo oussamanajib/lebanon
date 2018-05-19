150181e0bf0f49ce1e7be863bc6fb8cce8757557
oussamanajib <39277805+oussamanajib@users.noreply.github.com>
2018-05-18 23:26:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<head>
+
+
+
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<meta name="author" content="" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="js.js"></script>
+
+<title>عالم مطورى الويب لتعليم التصميم والبرمجة </title>
+
+</head>
+<body>
+
+		<div id="wrapper">
+
+<?php include('includes/header.php'); ?>
+
+<?php include('includes/nav.php'); ?>
+
+<div id="content">
+
+<a href="#"><h3>||المقـالات المضافة حديثا||</h1></a>
+
+<?php
+
+include("article.php");
+
+?>
+
+</div>
+
+
+<?php include('includes/sidebar.php'); ?>
+
+<?php include('includes/footer.php'); ?>
+
+		</div>
+
+	</body>
+
+</html>
